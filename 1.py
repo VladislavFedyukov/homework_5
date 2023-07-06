afdfb9ae3ac5c7036630212edeef2252cb393124
Vladislav <vladislav.fedyukov@gmail.com>
2023-07-06 09:32:03 +0300

    Добавление кода

--- 1.py
+++ 1.py
@@ -2,3 +2,16 @@
 # Функция возвращает кортеж из трёх элементов: 
 # путь, имя файла, расширение файла.
 
+abs_path = r'C:\Users\User\Desktop\homework_5\статья.txt'         
+
+
+def split_path(abs_path: str) -> tuple():
+    list_abs_path = abs_path.split('\\')
+    list_last_elem = list_abs_path[-1].split('.')
+    path = '\\'.join(list_abs_path[0:-1])
+    name = list_last_elem[0]
+    expansion = list_last_elem[1]
+    return path, name, expansion
+
+
+print(split_path(abs_path))
